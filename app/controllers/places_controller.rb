98f197140afd11f7ebaadbdfb88f44bb99445526
hyon lee <hyon.lee@utexas.edu>
2019-06-20 19:37:37 +0000

    heroku debug4

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create]
 
 
   def index
@@ -12,13 +12,13 @@ class PlacesController < ApplicationController
   end  
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
 private
 
-def place_params
-  params.require(:place).permit(:name, :description, :address)  
-end
+  def place_params
+    params.require(:place).permit(:name, :description, :address)  
+  end
 end
