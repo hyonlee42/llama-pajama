477a58eafbb4d3647c25eee2d40ffd00cb758fc2
hyon lee <hyon.lee@utexas.edu>
2019-06-21 19:02:31 +0000

    edit permissions 2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
 
   def index
@@ -23,8 +23,9 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
 
-    if @place.user != current_user
-      return render plain: 'Not Allwed', status: forbidden
+      if @place.user != current_user
+        return render plain: 'Not Allowed', status: :forbidden
+      end  
   end  
 
   def update
@@ -44,4 +45,5 @@ private
   def place_params
     params.require(:place).permit(:name, :description, :address)  
   end
+
 end
