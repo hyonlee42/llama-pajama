bf2d96bcad0497950933fd0b909f0750fc44d2bb
hyon lee <hyon.lee@utexas.edu>
2019-06-24 23:05:22 +0000

    email restart

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,7 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
-
-  def email
-    @email = email
-  end
-
   def comment_added
     mail(to: "stopthesignups@gmail.com",
-         subject: "A comment has been added to your place")
+             subject: "A comment has been added to your place")
   end
 end
